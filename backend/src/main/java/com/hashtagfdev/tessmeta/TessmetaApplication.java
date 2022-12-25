e0af82a3dcfbcc6f69867fd7be2e6c5216607d6b
Felipe Buovo <felipe_buovo2@hotmail.com>
2022-12-25 19:29:52 -0300

    Criação do projeto

---
+++ backend/src/main/java/com/hashtagfdev/tessmeta/TessmetaApplication.java
@@ -0,0 +1,13 @@
+package com.hashtagfdev.tessmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TessmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TessmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hashtagfdev/tessmeta/TessmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashtagfdev.tessmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TessmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
